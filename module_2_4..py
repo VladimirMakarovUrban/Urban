8f66cc9b7036088f361885d7d9cb4da0c704dae0
VladimirMakarov <166584499+VladimirMakarovUrban@users.noreply.github.com>
2024-10-29 19:28:14 +0500

    Add files via upload

--- module_2_4..py
+++ module_2_4..py
@@ -6,16 +6,18 @@
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes = []
 not_primes = []
-list_divider = []
 
 for num in numbers:
+    is_prime = True
     if num > 1:
         for i in range(2, num):
             if (num % i) == 0:
-                not_primes.append(num)
+                is_prime = False
                 break
-        else:
+        if is_prime:
             primes.append(num)
+        else:
+            not_primes.append(num)
     else:
         not_primes.append(num)
 print('primes', primes)
