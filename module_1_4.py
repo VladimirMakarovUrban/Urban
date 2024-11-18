d4a25c538b5d9e244b61e4645b4df0c341bc0012
VladimirMakarov <166584499+VladimirMakarovUrban@users.noreply.github.com>
2024-11-18 21:11:47 +0500

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,8 @@
+my_string = input('Введите чего-нибудь:')
+print(my_string)
+print('Количество символов: ', len(my_string))
+print('Верхний регистр: ', my_string.upper())
+print('Нижний регистр: ', my_string.lower())
+print('Без пробелов: ', my_string.replace(' ', ''))
+print('Первый символ: ', my_string[0])
+print('Последний символ: ', my_string[-1])
