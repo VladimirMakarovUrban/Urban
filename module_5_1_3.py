7170733be86228cbd2b9836ddc9efb14665e7cab
VladimirMakarov <166584499+VladimirMakarovUrban@users.noreply.github.com>
2024-12-02 08:36:45 +0500

    Update module_5_1_3.py

--- module_5_1_3.py
+++ module_5_1_3.py
@@ -2,16 +2,19 @@ class House:
     houses_history = []
 
     def __new__(cls, *args, **kwargs):
-        return object.__new__(cls)
+        # return object.__new__(cls)
+        cls.houses_history.append(args[0])  # Добавляем название дома в историю при создании объекта
+        return super().__new__(cls)
 
     def __init__(self, name, number_of_floors):
         self.name = name
         self.number_of_floors = number_of_floors
-        self.houses_history.append(name)
+        # self.houses_history.append(name)
 
     def __del__(self):
-        self.houses_history.remove(self.name)
-        return print(f"{self.name} снесён, но он останется в истории")
+        # self.houses_history.remove(self.name)
+        # return print(f"{self.name} снесён, но он останется в истории")
+        print(f"{self.name} снесён, но он останется в истории")
 
 
 h1 = House('ЖК Эльбрус', 10)
