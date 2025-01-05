83d755914e9a220f19f550c544073feba854a113
VladimirMakarov <166584499+VladimirMakarovUrban@users.noreply.github.com>
2025-01-05 18:23:57 +0500

    Add files via upload

---
+++ module_10_1.py
@@ -0,0 +1,42 @@
+import time
+from time import sleep
+from threading import Thread
+
+def write_words(word_count, file_name):
+
+    with open(file_name, 'w', encoding='UTF-8') as file:
+        for i in range(1, word_count+1):
+            file.write(f'Какое-то слово № {i}\n')
+            sleep(0.1)
+    print(f'Завершилась запись в файл {file_name}')
+
+
+start_time = time.time()
+write_words(10, 'example1.txt')
+write_words(30, 'example2.txt')
+write_words(200, 'example3.txt')
+write_words(100, 'example4.txt')
+end_time = time.time()
+all_time = end_time - start_time
+print(all_time)
+
+t1 = Thread(target=write_words, args=(10, 'example5.txt'))
+t2 = Thread(target=write_words, args=(30, 'example6.txt'))
+t3 = Thread(target=write_words, args=(200, 'example7.txt'))
+t4 = Thread(target=write_words, args=(100, 'example8.txt'))
+
+start_time = time.time()
+
+t1.start()
+t2.start()
+t3.start()
+t4.start()
+
+t1.join()
+t2.join()
+t3.join()
+t4.join()
+
+end_time = time.time()
+all_time = end_time - start_time
+print(all_time)
